ffc1639c67868fb2b74bbed6e4c2e84b50e1efef
Bradley Lindquist <bradleylindquist@Bradleys-Mac-mini.local>
2024-01-24 20:53:10 -0500

    added quiz questions and answers

---
+++ questions.js
@@ -0,0 +1,36 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What is HTML",
+    answer: "B. Hypertext Markup Language",
+    options: ["A. Hyper Type Multi Language", "B. Hypertext Markup Language", "C. Historic Trust Moving Language"],
+  },
+
+  {
+    numb: 2,
+    question: "Virgil?",
+    answer: "A. Van Dijk",
+    options: ["A. Van Dijk", "B. Van Dike", "C. Van Dyke"],
+  },
+
+  {
+    numb: 3,
+    question: "Jurgen?",
+    answer: "A. Klopp",
+    options: ["A. Klopp", "B. Clop", "C. Clopp"],
+  },
+
+  {
+    numb: 4,
+    question: "Trent?",
+    answer: "A. Alexander-Arnold",
+    options: ["A. Alexander-Arnold", "B. Alex-Arnie", "C. Arnold-Alexander"],
+  },
+
+  {
+    numb: 5,
+    question: "Mo?",
+    answer: "A. Salah",
+    options: ["A. Salah", "B. Sala", "C. Salaw"],
+  },
+];
