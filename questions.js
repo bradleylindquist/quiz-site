22176a07beb25c8ea6d53f31c3b1947024c99680
Bradley Lindquist <bradleylindquist@Bradleys-Mac-mini.local>
2024-02-01 23:23:46 -0500

    updated questions

--- questions.js
+++ questions.js
@@ -1,36 +1,75 @@
 let questions = [
   {
     numb: 1,
-    question: "What is HTML",
-    answer: "B. Hypertext Markup Language",
-    options: ["A. Hyper Type Multi Language", "B. Hypertext Markup Language", "C. Historic Trust Moving Language"],
+    question: "What is Oumuamua?",
+    answer: "B. Interstellar Object that passed through the Solar System in 2017",
+    options: [
+      "A. Massive black hole in Horsehead Nebula",
+      "B. Interstellar Object that passed through the Solar System in 2017",
+      "C. Comet in the Kuiper Belt",
+    ],
   },
 
   {
     numb: 2,
-    question: "Virgil?",
-    answer: "A. Van Dijk",
-    options: ["A. Van Dijk", "B. Van Dike", "C. Van Dyke"],
+    question: "Which Planet has a hexagon at its north pole?",
+    answer: "A. Saturn",
+    options: ["A. Saturn", "B. Neptune", "C. Venus"],
   },
 
   {
     numb: 3,
-    question: "Jurgen?",
-    answer: "A. Klopp",
-    options: ["A. Klopp", "B. Clop", "C. Clopp"],
+    question: "What is best described as a 'Luminous Active Galactic Nucleus?",
+    answer: "B. Quasar",
+    options: ["A. Megatron", "B. Quasar", "C. White Dwarf Star"],
   },
 
   {
     numb: 4,
-    question: "Trent?",
-    answer: "A. Alexander-Arnold",
-    options: ["A. Alexander-Arnold", "B. Alex-Arnie", "C. Arnold-Alexander"],
+    question: "This is the supervoid whose center is approximately 700M light-years from Earth",
+    answer: "B. Bootes Void",
+    options: ["A. Steppes Void", "B. Bootes Void", "C. Cloggs Void"],
   },
 
   {
     numb: 5,
-    question: "Mo?",
-    answer: "A. Salah",
-    options: ["A. Salah", "B. Sala", "C. Salaw"],
+    question: "The Largest Structure in the observable universe?",
+    answer: "A. Hercules-Corona Borealis Great Wall",
+    options: ["A. Hercules-Corona Borealis Great Wall", "B. Southern Polar Great Wall", "C. Swift-Gamma Great Wall"],
+  },
+
+  {
+    numb: 6,
+    question: "The closest extrasolar star to Earth?",
+    answer: "A. Proxima Centauri",
+    options: ["A. Proxima Centauri", "B. Barnard", "C. Wolf 359"],
+  },
+
+  {
+    numb: 7,
+    question: "The closest Galaxy to the Milky Way?",
+    answer: "B. Andromeda",
+    options: ["A. Pinwheel", "B. Andromeda", "C. Whirlpool"],
+  },
+
+  {
+    numb: 8,
+    question: "The vast cloud of icy planetesimals surrounding the sun?",
+    answer: "A. Oort cloud",
+    options: ["A. Oort cloud", "B. Reflection nebula", "C. Cosmic ocean"],
+  },
+
+  {
+    numb: 9,
+    question: "How many galaxies in the observable universe?",
+    answer: "C. 2 trillion",
+    options: ["A. 500 billion", "B. 1 trillion", "C. 2 trillion"],
+  },
+
+  {
+    numb: 10,
+    question: "The second most-famous dwarf planet in the solar system?",
+    answer: "A. Ceres",
+    options: ["A. Ceres", "B. Iris", "C. Loris"],
   },
 ];
